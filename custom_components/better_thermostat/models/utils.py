e59d41fa75f7b23c546074e36f0e5b09593e4bfd
Tobias Haber <kontakt@t-haber.de>
2022-02-20 17:16:27 +0100

    [TASK] fix temperature calibration

--- custom_components/better_thermostat/models/utils.py
+++ custom_components/better_thermostat/models/utils.py
@@ -26,14 +26,14 @@ def calibration(self, calibration_type):
 		return default_calibration(self)
 
 def default_calibration(self):
-	# FIXME: Write docstring
+	# This calibration is for devices with local calibration option, it syncs the current temperature of the TRV to the target temperature of the external sensor.
 	
 	state = self.hass.states.get(self.heater_entity_id).attributes
 	new_calibration = float((float(self._cur_temp) - float(state.get('current_temperature'))) + float(state.get('local_temperature_calibration')))
 	return convert_decimal(new_calibration)
 
 def temperature_calibration(self):
-	# FIXME: Write docstring
+	# This calibration is for devices with no local calibration option, it syncs the target temperature of the TRV to a new target temperature based on the current temperature of the external sensor.
 	
 	state = self.hass.states.get(self.heater_entity_id).attributes
 	if not all([self._target_temp, self._cur_temp, state.get('current_temperature')]):
@@ -41,7 +41,7 @@ def temperature_calibration(self):
 			return float(self._TRV_min_temp)
 		return float(self._target_temp)
 	else:
-		new_calibration = abs(float(round((float(self._target_temp) - float(self._cur_temp)) + float(state.get('current_temperature')), 2)))
+		new_calibration = float(round((float(self._target_temp) - float(self._cur_temp)) + float(state.get('current_temperature')), 2))
 		if new_calibration < float(self._TRV_min_temp):
 			new_calibration = float(self._TRV_min_temp)
 		if new_calibration > float(self._TRV_max_temp):
