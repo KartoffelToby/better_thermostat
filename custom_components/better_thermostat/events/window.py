a93c8b00a42ed920b98aafe0419169182ed5accb
@RubenKelevra <cyrond@gmail.com>
2022-02-22 16:40:45 +0100

    improve decoding of window state
    - assume the window is open if "unknown" is reported (sensor is offline)
    - do not change the state if sensor data is not recognized

--- custom_components/better_thermostat/events/window.py
+++ custom_components/better_thermostat/events/window.py
@@ -15,8 +15,14 @@ async def trigger_window_change(self):
 		check = self.hass.states.get(self.window_sensors_entity_ids).state
 		if check == 'on':
 			self.window_open = True
-		else:
+		elif check == 'off':
 			self.window_open = False
+		elif check == 'unknown':
+			self.window_open = True
+			_LOGGER.warning("better_thermostat %s: Window sensor state is unknown, assuming window is open", self.name)
+		else:
+			_LOGGER.error("better_thermostat %s: New window sensor state not recognized", self.name)
+			return
 		_LOGGER.debug("better_thermostat %s: Window (group) state changed to %s", self.name, "open" if self.window_open else "closed")
 		self.async_write_ha_state()
 		await control_trv(self)
