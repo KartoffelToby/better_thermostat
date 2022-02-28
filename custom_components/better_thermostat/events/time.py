2bd7e699a2cebbbf6b0ae49c2ae6adc3e66eec19
@RubenKelevra <cyrond@gmail.com>
2022-02-28 22:31:04 +0100

    convert docstrings to numpy in events.time

--- custom_components/better_thermostat/events/time.py
+++ custom_components/better_thermostat/events/time.py
@@ -9,11 +9,18 @@ _LOGGER = logging.getLogger(__name__)
 
 @callback
 async def trigger_time(self, current_time):
-	"""
-	Triggered by night mode timer.
-	@param current_time:
-	"""
+	"""Triggered by night mode timer.
 	
+	Parameters
+	----------
+	self : 
+	current_time :
+		time.time()
+
+	Returns
+	-------
+
+	"""
 	_is_night = _nighttime(self, current_time)
 	
 	if _is_night is None:
@@ -39,10 +46,18 @@ async def trigger_time(self, current_time):
 
 @callback
 def _nighttime(self, current_time):
-	"""
-	Return whether it is nighttime.
-	@param current_time: time.time()
-	@return: bool True if it is nighttime; True if not configured
+	"""Checks whether it is currently nighttime
+	
+	Parameters
+	----------
+	self : 
+	current_time :
+		time.time()
+
+	Returns
+	-------
+	bool
+		True if it is nighttime; True if not configured
 	"""
 	_return_value = None
 	
