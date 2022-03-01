5d7d88f2b0099d4b5e1d34c95b818481cef16bb6
@RubenKelevra <cyrond@gmail.com>
2022-03-01 18:46:47 +0100

    rework events.trv
    - remove local dictionary with changes
    - add type checks
    - do system mode changes in any case (we might have ignored previous
    updates because of startup or ignore_states
    - remove check for valve updates (never part of this status updates)

--- custom_components/better_thermostat/events/trv.py
+++ custom_components/better_thermostat/events/trv.py
@@ -1,9 +1,8 @@
 import logging
 
 from homeassistant.components.climate.const import HVAC_MODE_HEAT, HVAC_MODE_OFF
-from homeassistant.core import callback
+from homeassistant.core import callback, State
 
-from ..const import ATTR_VALVE_POSITION
 from ..controlling import control_trv
 from ..models.models import convert_inbound_states
 from ..models.utils import convert_to_float
@@ -27,49 +26,39 @@ async def trigger_trv_change(self, event):
 		_LOGGER.debug(f"better_thermostat {self.name}: TRV update contained not all necessary data for processing, skipping")
 		return
 	
-	remapped_state = convert_inbound_states(self, new_state)
+	if not isinstance(new_state, State) or not isinstance(old_state, State):
+		_LOGGER.debug(f"better_thermostat {self.name}: TRV update contained not a State, skipping")
+		return
 	
-	update_valve_position(self, remapped_state.get(ATTR_VALVE_POSITION))
+	try:
+		convert_inbound_states(self, new_state)
+	except TypeError:
+		_LOGGER.debug(f"better_thermostat {self.name}: remapping TRV state failed, skipping")
+		return
 	
 	# if flag is on, we won't do any further processing
 	if self.ignore_states:
 		_LOGGER.debug(f"better_thermostat {self.name}: skipping trigger_trv_change because ignore_states is true")
 		return
 	
-	# system mode has been changed
-	if old_state != new_state:
-		_LOGGER.debug(
-			f"better_thermostat {self.name}: TRV System Mode changed from '{old_state}' to '{new_state}'"
-		)
-		
-		new_decoded_system_mode = remapped_state.get('system_mode')
-		_LOGGER.debug(
-			f"better_thermostat {self.name}: Expected decoded TRV mode: '{self._hvac_mode}'. TRV's decoded TRV mode: '{new_decoded_system_mode}'"
-		)
-		
-		if new_decoded_system_mode is None or new_decoded_system_mode not in (HVAC_MODE_OFF, HVAC_MODE_HEAT):
-			# we don't understand this system mode, so we'll force an update to overwrite it
-			_LOGGER.debug(f"better_thermostat {self.name}: TRV's decoded TRV mode is not valid, overwriting with correct system mode")
-			
-			force_update = True
-			_LOGGER.debug(
-				f"better_thermostat {self.name}: Could not parse new system mode from TRV, forcing an update with the "
-				f"expected system mode: {self._hvac_mode}"
-			)
-		
-		# check if this change is what we expected (if not already an update is forced)
-		if not force_update and self._hvac_mode != new_decoded_system_mode:
-			_LOGGER.warning(
-				f"better_thermostat {self.name}: TRV is not in the expected mode, we will force an update"
-			)
-			force_update = True
-		
-		if force_update:
-			await control_trv(self)
-			return
+	new_decoded_system_mode = str(new_state.state)
 	
-	else:
-		_LOGGER.debug(f"better_thermostat {self.name}: trigger_trv_change detected no system mode change")
+	if new_decoded_system_mode not in (HVAC_MODE_OFF, HVAC_MODE_HEAT):
+		# not an valid mode, overwriting
+		_LOGGER.debug(f"better_thermostat {self.name}: TRV's decoded TRV mode is not valid, overwriting with correct system mode")
+		
+		force_update = True
+	
+	# check if this change is what we expected (if not already an update is forced)
+	if not force_update and self._hvac_mode != new_decoded_system_mode:
+		_LOGGER.warning(
+			f"better_thermostat {self.name}: TRV is not in the expected mode, we will force an update"
+		)
+		force_update = True
+	
+	if force_update:
+		await control_trv(self)
+		return
 	
 	# we only read user input at the TRV on mode 0
 	if self.calibration_type != 0:
@@ -79,11 +68,8 @@ async def trigger_trv_change(self, event):
 	if self._hvac_mode == HVAC_MODE_OFF:
 		return
 	
-	if _new_heating_setpoint := convert_to_float(
-		new_state.attributes.get('current_heating_setpoint'),
-		self.name,
-		"trigger_trv_change()"
-	) is None:
+	if _new_heating_setpoint := convert_to_float(new_state.attributes.get('current_heating_setpoint'), self.name, "trigger_trv_change()") \
+	                            is None:
 		return
 	
 	if _new_heating_setpoint < self._min_temp or self._max_temp < _new_heating_setpoint:
@@ -114,6 +100,10 @@ def update_valve_position(self, valve_position):
 	-------
 	None 
 	"""
+	
 	if valve_position is not None:
+		_LOGGER.debug(f"better_thermostat {self.name}: Updating valve position to {valve_position}")
 		self._last_reported_valve_position = valve_position
 		self._last_reported_valve_position_update_wait_lock.release()
+	else:
+		_LOGGER.debug(f"better_thermostat {self.name}: Valve position is None, skipping")
