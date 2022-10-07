bfc7be7746bc5e8e6ec4a2f81839878d9024f620
KartoffelToby <kontakt@t-haber.de>
2022-10-07 21:57:29 +0200

    [TASK9 plain

--- custom_components/better_thermostat/__init__.py
+++ custom_components/better_thermostat/__init__.py
@@ -2,9 +2,14 @@
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant, Config
 from homeassistant.config_entries import ConfigEntry
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "better_thermostat"
 PLATFORMS = [Platform.CLIMATE]
+
+_LOGGER.error("Better Thermostat is not stable release yet. Please use a Beta Version.")
 
 
 async def async_setup(hass: HomeAssistant, config: Config):
@@ -13,23 +18,4 @@ async def async_setup(hass: HomeAssistant, config: Config):
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
-    entry.async_on_unload(entry.add_update_listener(config_entry_update_listener))
-    return True
-
-
-async def config_entry_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Handle options update."""
-    await async_unload_entry(hass, entry)
-    await async_setup_entry(hass, entry)
-
-
-async def async_unload_entry(hass, entry):
-    """Unload a config entry."""
-    await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    return True
-
-
-async def async_reload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
-    await async_unload_entry(hass, config_entry)
-    await async_setup_entry(hass, config_entry)
+    return False
