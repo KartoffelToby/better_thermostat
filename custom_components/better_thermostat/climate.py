351739113f40246a46e9119dd96dcc16e9db14e0
KartoffelToby <kontakt@t-haber.de>
2022-11-13 21:14:36 +0100

    [TASK] fix startup

--- custom_components/better_thermostat/climate.py
+++ custom_components/better_thermostat/climate.py
@@ -250,7 +250,6 @@ class BetterThermostat(ClimateEntity, RestoreEntity, ABC):
             if trv["advanced"]["calibration"] == "local_calibration_based":
                 _calibration = 0
             _adapter = load_adapter(self, trv["integration"], trv["trv"])
-            await _adapter.init(self, trv)
             self.real_trvs[trv["trv"]] = {
                 "calibration": _calibration,
                 "integration": trv["integration"],
@@ -274,8 +273,9 @@ class BetterThermostat(ClimateEntity, RestoreEntity, ABC):
                 "last_valve_position": None,
                 "last_hvac_mode": None,
                 "last_current_temperature": None,
-                "last_calibration": await _adapter.get_current_offset(self, trv["trv"]),
+                "last_calibration": None,
             }
+            await _adapter.init(self, trv)
 
         await super().async_added_to_hass()
 
@@ -606,6 +606,9 @@ class BetterThermostat(ClimateEntity, RestoreEntity, ABC):
 
             _all_trvs = []
             for trv in self.real_trvs.keys():
+                self.real_trvs[trv["trv"]]["last_calibration"] = await self.real_trvs[
+                    trv["trv"]
+                ]["adapter"].get_current_offset(self, trv["trv"])
                 self.real_trvs[trv]["last_hvac_mode"] = self.hass.states.get(trv).state
                 self.real_trvs[trv]["last_temperature"] = convert_to_float(
                     str(self.hass.states.get(trv).attributes.get("temperature")),
