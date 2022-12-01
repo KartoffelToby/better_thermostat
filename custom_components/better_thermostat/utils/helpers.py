62081eb3bcc8567a08cf8b7738e180d0907a5fea
Wolfgang Tom <mail@wolfgangtom.de>
2022-12-01 09:35:41 +0100

    revert to 2.5 degrees, some people have problems with too low values

--- custom_components/better_thermostat/utils/helpers.py
+++ custom_components/better_thermostat/utils/helpers.py
@@ -149,8 +149,8 @@ def calculate_setpoint_override(self, entity_id) -> Union[float, None]:
 
     if self.real_trvs[entity_id]["advanced"].get("fix_calibration", False) is True:
         _temp_diff = float(float(self.bt_target_temp) - float(self.cur_temp))
-        if _temp_diff > 0.0 and _calibrated_setpoint - _cur_trv_temp < 1.5:
-            _calibrated_setpoint += 1.5
+        if _temp_diff > 0.0 and _calibrated_setpoint - _cur_trv_temp < 2.5:
+            _calibrated_setpoint += 2.5
 
     # check if new setpoint is inside the TRV's range, else set to min or max
     if _calibrated_setpoint < self.real_trvs[entity_id]["min_temp"]:
