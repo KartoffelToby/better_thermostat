19d0ec4613c259ff4cea2d89810b8c08609df9fb
Wolfgang Tom <mail@wolfgangtom.de>
2022-12-06 13:00:59 +0100

    linting

--- custom_components/better_thermostat/model_fixes/TS0601.py
+++ custom_components/better_thermostat/model_fixes/TS0601.py
@@ -10,6 +10,9 @@ def fix_target_temperature_calibration(self, entity_id, temperature):
     )
     if _cur_trv_temp is None:
         return temperature
-    if round(temperature, 1) > round(_cur_trv_temp, 1) and temperature - _cur_trv_temp < 1.5:
+    if (
+        round(temperature, 1) > round(_cur_trv_temp, 1) 
+        and temperature - _cur_trv_temp < 1.5
+    ):
         temperature += 1.5
     return temperature
