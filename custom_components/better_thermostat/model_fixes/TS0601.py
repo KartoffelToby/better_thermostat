aefba58aa6e61a2e4a9602d20ebfe4851ac6bb97
Wolfgang Tom <mail@wolfgangtom.de>
2022-12-06 12:13:05 +0100

    TS0601 quirk

---
+++ custom_components/better_thermostat/model_fixes/TS0601.py
@@ -0,0 +1,15 @@
+def fix_local_calibration(self, entity_id, offset):
+    if offset > -1.5:
+        offset -= 1.5
+    return offset
+
+
+def fix_target_temperature_calibration(self, entity_id, temperature):
+    _cur_trv_temp = float(
+        self.hass.states.get(entity_id).attributes["current_temperature"]
+    )
+    if _cur_trv_temp is None:
+        return temperature
+    if temperature - _cur_trv_temp < 1.5:
+        temperature += 1.5
+    return temperature
