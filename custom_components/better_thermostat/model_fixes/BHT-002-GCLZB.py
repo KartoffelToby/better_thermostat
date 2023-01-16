24325c783ae52629ce9beb21220203b7b0b09f6b
Oliver Butler <dev@oliverbutler.uk>
2023-01-16 18:55:59 +0000

    Inverse the offset rounding

--- custom_components/better_thermostat/model_fixes/BHT-002-GCLZB.py
+++ custom_components/better_thermostat/model_fixes/BHT-002-GCLZB.py
@@ -1,17 +1,20 @@
 """
 This model fix is due to any floating point number being set to +/- 1 million by Zigbee2MQTT for the local_calibration
-
 """
 
 import math
 
 
 def fix_local_calibration(self, entity_id, offset):
-    # If still heating, round down
-    if self.cur_temp <= self.bt_target_temp:
-        offset = math.floor(offset)
-    else:
+    """
+    If still heating, round UP the offset
+    
+    This creates a lower "fake" thermostat temperature, making it heat the room
+    """
+    if self.cur_temp < self.bt_target_temp:
         offset = math.ceil(offset)
+    else:
+        offset = math.floor(offset)
 
     return offset
 
