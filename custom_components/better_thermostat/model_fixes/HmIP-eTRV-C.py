c081421e1284e730da969fa5d5edb80362c230ba
Hidri02 <68425351+Hidri02@users.noreply.github.com>
2023-11-11 16:44:34 +0100

    fix indentation

--- custom_components/better_thermostat/model_fixes/HmIP-eTRV-C.py
+++ custom_components/better_thermostat/model_fixes/HmIP-eTRV-C.py
@@ -2,11 +2,11 @@ def fix_local_calibration(self, entity_id, offset):
     return offset
 
 def fix_target_temperature_calibration(self, entity_id, temperature):
-	"""
-	Fixes endless heating loops caused by homeaticIP device algorithm heating already when temperature is not below the set temperature yet. 
-	Adds a bigger delta of at least -1.5 when not trying to heat.
     """
-
+    Fixes endless heating loops caused by homeaticIP device algorithm heating already when temperature is not below the set temperature yet. 
+    Adds a bigger delta of at least -1.5 when not trying to heat.
+    """
+    
     _cur_trv_temp = float(
         self.hass.states.get(entity_id).attributes["current_temperature"]
     )
@@ -14,7 +14,7 @@ def fix_target_temperature_calibration(self, entity_id, temperature):
         return temperature
     if temperature < _cur_trv_temp:
         temperature -= 1.5
-
+    
     return temperature
 
 async def override_set_hvac_mode(self, entity_id, hvac_mode):
