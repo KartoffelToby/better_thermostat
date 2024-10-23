9db072671b482b6a83a7f3be03d069153299b431
Dr. Torge Valerius <2878247+cygnusb@users.noreply.github.com>
2024-10-23 16:26:15 +0200

    Model quirk For Bosch BTH-RM230Z room thermostat
    This fixes handling of set_temperature, if device is used in newer
    Z2M, which requires to set target_temp_high and target_temp_low
    instead of temperature.
    Backward compatiblity is ensured by checking model, platform (mqtt),
    and if heat/cool hvac_modes are present.
    Fixes KartoffelToby/better_thermostat#1401

---
+++ custom_components/better_thermostat/model_fixes/BTH-RM230Z.py
@@ -0,0 +1,53 @@
+# Quirks for BTH-RM230Z
+import logging
+from homeassistant.helpers import device_registry as dr, entity_registry as er
+
+_LOGGER = logging.getLogger(__name__)
+
+def fix_local_calibration(self, entity_id, offset):
+    return offset
+
+
+def fix_target_temperature_calibration(self, entity_id, temperature):
+    return temperature
+
+
+async def override_set_hvac_mode(self, entity_id, hvac_mode):
+    return False
+
+
+async def override_set_temperature(self, entity_id, temperature):
+    """Bosch room thermostat BTH-RM230Z has a quirk where it needs to set both high 
+    and low temperature, if heat and cool modes are available in newer Z2M versions.
+    """
+    model = self.real_trvs[entity_id]["model"]
+    if model == "BTH-RM230Z":
+        _LOGGER.debug(
+            f"better_thermostat {self.name}: TRV {entity_id} device quirk bth-rm230z for set_temperature active"
+        )
+        entity_reg = er.async_get(self.hass)
+        entry = entity_reg.async_get(entity_id)
+
+        hvac_modes = entry.capabilities.get("hvac_modes", [])
+
+        _LOGGER.debug(
+            f"better_thermostat {self.name}: TRV {entity_id} device quirk bth-rm230z found hvac_modes {hvac_modes}"
+        )
+
+        if entry.platform == "mqtt" and "cool" in hvac_modes and "heat" in hvac_modes:
+            await self.hass.services.async_call(
+                "climate",
+                "set_temperature",
+                {"entity_id": entity_id, "target_temp_high": temperature, "target_temp_low": temperature}, 
+                blocking=True,
+                context=self.context,
+            )
+        else:
+            await self.hass.services.async_call(
+                "climate",
+                "set_temperature",
+                {"entity_id": entity_id, "temperature": temperature},
+                blocking=True,
+                context=self.context,
+            )
+    return True
