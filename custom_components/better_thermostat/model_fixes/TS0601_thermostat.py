468c14bd1ea8cee419a06293f304adfbfca4ca33
@RubenKelevra <cyrond@gmail.com>
2024-11-15 18:26:56 +0100

    [TS0601_thermostat]: Remove not working fix from local calibration

--- custom_components/better_thermostat/model_fixes/TS0601_thermostat.py
+++ custom_components/better_thermostat/model_fixes/TS0601_thermostat.py
@@ -1,22 +1,11 @@
 def fix_local_calibration(self, entity_id, offset):
-    _cur_external_temp = self.cur_temp
-    _target_temp = self.bt_target_temp
-
-    if (_cur_external_temp + 0.1) >= _target_temp:
-        offset = round(offset + 0.5, 1)
-    elif (_cur_external_temp + 0.5) >= _target_temp:
-        offset -= 2.5
-
     return offset
-
 
 def fix_target_temperature_calibration(self, entity_id, temperature):
     return temperature
 
-
 async def override_set_hvac_mode(self, entity_id, hvac_mode):
     return False
-
 
 async def override_set_temperature(self, entity_id, temperature):
     return False
