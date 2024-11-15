a7f74ae0531dcd4857b4bc2bfb9b4fd25706f41e
@RubenKelevra <cyrond@gmail.com>
2024-11-15 18:09:51 +0100

    [TS0601_thermostat] Remove "fix" which selects too high setpoints
    - removes "fix" which leads too high set points and significant overheating of rooms

--- custom_components/better_thermostat/model_fixes/TS0601_thermostat.py
+++ custom_components/better_thermostat/model_fixes/TS0601_thermostat.py
@@ -11,17 +11,6 @@ def fix_local_calibration(self, entity_id, offset):
 
 
 def fix_target_temperature_calibration(self, entity_id, temperature):
-    _cur_trv_temp = float(
-        self.hass.states.get(entity_id).attributes["current_temperature"]
-    )
-    if _cur_trv_temp is None:
-        return temperature
-    if (
-        round(temperature, 1) > round(_cur_trv_temp, 1)
-        and temperature - _cur_trv_temp < 1.5
-    ):
-        temperature += 1.5
-
     return temperature
 
 
