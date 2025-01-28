e2d97e1d5aeb5aec877553623b9ceff3455928a2
albummi <110391540+albummi@users.noreply.github.com>
2025-01-28 12:29:05 +0100

    Update __init__.py

--- custom_components/better_thermostat/__init__.py
+++ custom_components/better_thermostat/__init__.py
@@ -37,7 +37,6 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     hass.data.setdefault(DOMAIN, {})
     return True
 
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Better Thermostat from a config entry."""
     # Fenster-Sensoren
@@ -62,7 +61,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     )
 
     return True
-
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
